490441f532c4e255003c20e6eba668e0ad553046
rachel y wang <wang.ryk@gmail.com>
2016-04-26 18:24:57 -0700

    updated code to correct script.js file

--- js/script.js
+++ js/script.js
@@ -36,3 +36,16 @@
 
 		};
 		console.log('hello there friends!');
+
+var doCoolStuff = function () {
+	var currentClassName = document.getElementById('cool').className;
+	
+	if(currentClassName === 'cool') {
+		document.getElementById('cool').className = 'cool red';
+	} else {
+		document.getElementById('cool').className = 'cool';
+	}
+	/* document = main window
+		changing class name
+	*/
+};
